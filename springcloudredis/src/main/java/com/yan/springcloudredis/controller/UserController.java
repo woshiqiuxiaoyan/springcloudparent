f4c5252efdba2fe0c4262759330b97455ce85c8d
chenbaojing <baojing.chen@51shaoxi.com>
2018-11-28 15:08:02 +0800

    mybatis整合

---
+++ springcloudredis/src/main/java/com/yan/springcloudredis/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.yan.springcloudredis.controller;
+
+/**
+ * @ClassName UserController
+ * @Author: ChenBJ
+ * @Description: TODO
+ * @Date: 2018/11/28 14:50
+ * @Version:
+ */
+public class UserController {
+}
