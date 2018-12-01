d12ea2179c69315f715058dd6faec326ebe0700c
丘小燕 <xiaoyan.qiu@51shaoxi.com>
2018-12-01 17:58:20 +0800

    加载 lua脚本

--- springcloudprovide/src/main/java/com/yan/springcloudprovide/SpringcloudprovideApplication.java
+++ springcloudprovide/src/main/java/com/yan/springcloudprovide/SpringcloudprovideApplication.java
@@ -3,6 +3,8 @@ package com.yan.springcloudprovide;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,9 +14,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("com.yan.springcloudprovide.mapper")
 @SpringBootApplication
 @EnableCaching
-public class SpringcloudprovideApplication {
+public class SpringcloudprovideApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudprovideApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringcloudprovideApplication.class);
+	}
+
+
 }
