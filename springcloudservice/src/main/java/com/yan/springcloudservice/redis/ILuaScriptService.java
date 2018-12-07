411577bf14fe3cdcd53f69deafbd010676a7dec6
丘小燕 <xiaoyan.qiu@51shaoxi.com>
2018-12-07 20:11:06 +0800

    extract common service

--- springcloudservice/src/main/java/com/yan/springcloudservice/redis/ILuaScriptService.java
+++ springcloudservice/src/main/java/com/yan/springcloudservice/redis/ILuaScriptService.java
@@ -4,21 +4,18 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * @author: zhongjie
- * date: 2018/7/18 0018
- * time: 17:37
- * description:处理redis lua脚本
+ * lua脚本编写
  */
 public interface ILuaScriptService {
 
-    public void eval(String luaKey,List<String> keys,List<String> args);
+    void eval(String luaKey,List<String> keys,List<String> args);
 
-    public String eval2string(String luaKey,List<String> keys,List<String> args);
+    String eval2string(String luaKey,List<String> keys,List<String> args);
 
-    public Map<String,String> eval2map(String luaKey,List<String> keys,List<String> args);
+    Map<String,String> eval2map(String luaKey,List<String> keys,List<String> args);
 
-    public <T> T eval2bean(String luaKey,List<String> keys,List<String> args,Class<T> clz);
+    <T> T eval2bean(String luaKey,List<String> keys,List<String> args,Class<T> clz);
 
-    public <T> List<T> eval2list(String luaKey,List<String> keys,List<String> args,Class<T> clz);
+    <T> List<T> eval2list(String luaKey,List<String> keys,List<String> args,Class<T> clz);
 
 }
